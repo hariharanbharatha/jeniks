bbad4482db2dc95558455ab44cc0db6ba40757e8
hariharanbharatha <hhariharan2525@gmail.com>
2025-06-12 08:51:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end('Hello from Jenkins + Docker!');
+  res.end('AM hariharan!');
 });
 
 server.listen(3000, () => {
